c58e49b207a4f94940b29e4e40a000eaf93df163
Yujun Won <yjwon@isecurekr.com>
2018-05-21 02:22:28 +0900

    form validatior 적용

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import *
 
+def min_length_3_validator(value):
+    if leng(value) < 3:
+        raise forms.ValidationError('3글자 이상 입력해주세요')
 
 class CategoryForm(forms.ModelForm):
 
@@ -12,6 +15,8 @@ class CategoryForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     # category = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control'}))
     # title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    # title = forms.CharField(widget=forms.TextInput(validators=[min_length_3_validator]))
+
     # content = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     # Tags = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     
