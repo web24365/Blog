ba69da2d8a2130944e440a72f5e2faa1b5796c0e
Yujun Won <yjwon@isecurekr.com>
2018-05-20 22:03:21 +0900

    root directory redirect blog, 3way

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,19 @@ from django.urls import path
 from django.conf.urls import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
+
+# def root(request):
+#    return redirect('blog:post_list')
 
 
 urlpatterns = [
+    # path('', root, name='root'),
+    # path('', lambda r: redirect('blog:post_list'), name='root'),
+    path('', RedirectView.as_view(pattern_name='blog:post_list')),
     path('admin/', admin.site.urls),
+    # namespcae를 지정하면 항상 사용해야한다. 사용하지 않으면 error발생
     path('blog/', include('blog.urls', namespace='blog')),
 ]
 
