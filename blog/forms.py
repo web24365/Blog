a221e5b75ce0bd4b9d9aa22ee7bbdb51a2bf08a2
Yujun Won <yjwon@isecurekr.com>
2018-05-23 03:44:58 +0900

    ModelForm#2

--- blog/forms.py
+++ blog/forms.py
@@ -16,8 +16,22 @@ class PostForm(forms.ModelForm):
     # title = forms.CharField(widget=forms.TextInput(validators=[min_length_3_validator]))
     # content = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     # Tags = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    content = forms.CharField(label='내용', widget=forms.TextInput(attrs={
-        'class': 'comment-form',
+    # def __init__(self, *args, **kwargs):
+    #     super(PostForm, self).__init__(*args, **kwargs)
+    #     self.fields['category'].widget.choices = Category.objects.all()
+
+    category = forms.ModelMultipleChoiceField(queryset=Category.objects.all().order_by('name'), widget=forms.Select(attrs={'class':'form-contrl',}))
+        
+    title = forms.CharField(label='제목', widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'help_text': '',
+    }))
+    content = forms.CharField(label='내용', widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'help_text': '100 characters max.',
+        'rows': 5,
+        'cols': 50,
+        'help_text': '글',
         'size': '70px',
         'placeholder': '', }))
     
